d7dc4ca7e3585a9920de10446f38bab241fd0d96
Meriem1551 <meriem200409@gmail.com>
2023-11-14 05:48:04 +0100

    Adding pointer to last node

--- first_task/push.c
+++ first_task/push.c
@@ -3,21 +3,27 @@
  * push - add node at the beginning of a queue
  * @Q: pointre to pointer to queue
  * @num: integer
+ * @tail: pointer to the last node
  * Return: void
  */
 
-void push(Queue **Q, int n)
+void push(Queue **Q, Queue **tail, int n)
 {
     Queue *new = (Queue *)malloc(sizeof(Queue));
     if (new == NULL)
         return;
     new->num = n;
     new->next = NULL;
+    new->prev = NULL;
     if (*Q == NULL)
+    {
         *Q = new;
+        *tail = new;
+    }
     else
     {
         new->next = *Q;
+        (*Q)->prev = new;
         *Q = new;
     }
 }
