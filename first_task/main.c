54fbe0941374948958e27af8aafd24a22e252db0
Meriem1551 <meriem200409@gmail.com>
2023-11-14 05:47:37 +0100

    Changing main

--- first_task/main.c
+++ first_task/main.c
@@ -11,14 +11,17 @@ int main(void)
     positif n_nodes, i;
     int number;
     Queue *q = NULL;
+    Queue *tail = NULL;
     printf("How many nodes do you have? ");
     scanf("%u", &n_nodes);
     for (i = 0; i < n_nodes; i++)
     {
         printf("Enter num number %u ", i);
         scanf("%d", &number);
-        push(&q, number);
+        push(&q, &tail, number);
     }
+    print(tail);
     free(q);
+    free(tail);
     return 0;
 }
