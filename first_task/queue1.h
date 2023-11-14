3832ce7aa8d0fca3eacb0d544354f40df1e772f5
Meriem1551 <meriem200409@gmail.com>
2023-11-14 05:46:55 +0100

    defining print and pop functions

--- first_task/queue1.h
+++ first_task/queue1.h
@@ -5,17 +5,22 @@
 #include <stdlib.h>
 typedef struct queue Queue;
 typedef unsigned int positif;
+
+// defining doubly linkd list
 /**
  * queue - struct
  * @num: integer
- * @next: pointer to s struct
+ * @next: pointer to struct (next node)
+ * @prev: pointer to struct (previous node)
  */
 struct queue
 {
     int num;
     struct queue *next;
+    struct queue *prev;
 };
 
-void push(Queue **, int);
-
+void push(Queue **, Queue **, int);
+void print(Queue *);
+void pop(Queue *);
 #endif
